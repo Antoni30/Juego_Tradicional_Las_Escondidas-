e9d2c8d39d1900e7da57dbdf9a9e083891bb45ee
Antoni30 <Antoni30@github.com>
2022-12-07 15:56:30 -0500

    update

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 from os import system
 import subprocess
-
-try:
-    opcion=-1
-    print("1.-Telefono Descompuesto")
-    print("2.-Escondidas")
-    opcion=int(input("Ingrese el juego que desea jugar "))
-except:
-    print("No es ninguna opccion")
-if(opcion==1):
-    subprocess.run('START "" "' + "[NRC_ 8001]_NicolasSuquillo_ProyectoUnidad1.py" + '"', shell=True)
-elif (opcion==2):
-    subprocess.run('START "" "' + "[NRC_8001]_AntoniToapanta_ProyectoUnidad1.py" + '"', shell=True)
-elif (opcion==-1):
-    print("ninguna opcion")
-
-system("pause")
+while True:
+    try:
+        print("\t\t\t\t\t------------------------------------------------------")
+        print("\t\t\t\t\t|Bienvenido a nuestra maquina de juegos Tradicionales|")
+        print("\t\t\t\t\t------------------------------------------------------\n")
+        opcion=-1
+        print("\t\t\t\t\t1.-Telefono Descompuesto")
+        print("\t\t\t\t\t2.-Escondidas")
+        print("\t\t\t\t\t0.-Salir")
+        opcion=int(input("\t\t\t\t\tIngrese el juego que desea jugar: "))
+    except:
+        print("No es ninguna opccion")
+    if(opcion==1):
+        subprocess.run('START "" "' + "[NRC_ 8001]_NicolasSuquillo_ProyectoUnidad1.py" + '"', shell=True)
+        system("cls")
+    elif (opcion==2):
+        subprocess.run('START "" "' + "[NRC_8001]_AntoniToapanta_ProyectoUnidad1.py" + '"', shell=True)
+        system("cls")
+    elif (opcion==-1):
+        system("cls")
+        print("no es una un numero")
+    elif opcion ==0:
+        break
+    system("pause")
